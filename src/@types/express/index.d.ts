5ea52570354249aa8913449e4d07a9c00155ce5a
Ranjith Bavithra <bavithraranjith24@example.com>
2026-02-17 11:15:43 +0530

    update current user error

---
+++ src/@types/express/index.d.ts
@@ -0,0 +1,22 @@
+/**
+ * @copyright 2025 Bavithra
+ * @license Apache-2.0
+ */
+
+/**
+ * Node modules
+ */
+import * as express from 'express';
+
+/**
+ * Types
+ */
+import type { Types } from 'mongoose';
+
+declare global {
+  namespace Express {
+    interface Request {
+      userId?: Types.ObjectId;
+    }
+  }
+}
---
+++ src/controllers/v1/user/get_current_user.ts
@@ -0,0 +1,42 @@
+/**
+ * @copyright 2025 Bavithra
+ * @license Apache-2.0
+ */
+
+/**
+ * Custom modules
+ */
+import { logger } from '@/lib/winston';
+
+/**
+ * Models
+ */
+
+import User from '@/models/user';
+
+/**
+ * Types
+ */
+
+import type { Request, Response } from 'express';
+
+const getCurrentUser = async (req: Request, res: Response): Promise<void> => {
+  try {
+    const userId = req.userId;
+
+    const user = await User.findById(userId).select('-_v').lean().exec();
+
+    res.status(200).json({
+      user,
+    });
+  } catch (err) {
+    res.status(500).json({
+      code: 'ServerError',
+      message: 'Internal server error',
+      error: err,
+    });
+    logger.error('Error while getting current user', err);
+  }
+};
+
+export default getCurrentUser;
